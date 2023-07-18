279403771aa3f8be5462e27daba3fe2d722242e3
Jakub Piksa <kuba.piksa@wp.pl>
2023-07-18 13:07:22 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,11 @@
 import css from './app.module.css'
-import user from './user/user.json';
-import friendsData from './friends/friends.json';
-import data from './data/data.json';
-import transactionsData from './transactions/transactions.json';
-import { Profile } from './user/user.jsx';
-import { FriendList } from './friends/friends.jsx';
-import { Statistics } from './data/data.jsx';
-import { TransactionHistory } from './transactions/transactions.jsx';
+
+
 
 function App() {
   return (
     <div className={css.body}>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <Statistics stats={data} />
-      <FriendList friends={friendsData} />
-      <TransactionHistory transactions={transactionsData} />
+      <h1>IS THIS WORKING??</h1>
     </div>
   );
 }
