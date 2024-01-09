aa8b68d5b342e729d9a73c50b6a50a29b3edcc0f
Jakub Piksa <kuba.piksa@wp.pl>
2024-01-09 12:43:55 +0100

    Update

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,26 +1,24 @@
 import React, { useState } from 'react';
-import css from "./searchbar.module.css"
-
+import css from './searchbar.module.css';
 
 const Searchbar = ({ onSubmit }) => {
   const [searchQuery, setSearchQuery] = useState('');
 
-  const handleInputChange = (event) => {
+  const handleInputChange = event => {
     setSearchQuery(event.target.value);
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = event => {
     event.preventDefault();
     onSubmit(searchQuery);
   };
 
   return (
     <header className={css.searchBar}>
+      <button type="submit" className={css.searchButton} onClick={handleSubmit}>
+        <span>Search</span>
+      </button>
       <form className={css.form} onSubmit={handleSubmit}>
-        <button type="submit" className={css.searchButton}>
-          <span>Search</span>
-        </button>
-
         <input
           className={css.input}
           type="text"
